d9546d4512af92f918d87f7b49bbbc99e06bf712
Alexander12Po <alexanderpaulhuamanramos25@gmail.com>
2026-07-30 11:04:04 -0500

    Actualizar estado.js

--- plugins/estado.js
+++ plugins/estado.js
@@ -11,18 +11,7 @@ export default {
       }, { quoted: msg });
     }
 
-    const jidNormal = `${numero}@s.whatsapp.net`;
-const jidLid = `${numero}@lid`;
-
-let cached = global.statusCache?.[jidNormal] || global.statusCache?.[jidLid];
-
-// Si no coincide exacto, busca por si el número está contenido en alguna clave
-if (!cached) {
-  const entrada = Object.entries(global.statusCache || {}).find(([key]) =>
-    key.includes(numero)
-  );
-  if (entrada) cached = entrada[1];
-}
+    const cached = global.statusCache?.[numero];
 
     if (!cached) {
       return sock.sendMessage(from, {
